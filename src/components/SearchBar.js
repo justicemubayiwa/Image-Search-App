edeb8bc1fe18707ce95da203869c6b097a62b906
justicemubayiwa <j.mubayiwa@gmail.com>
2021-12-01 22:12:56 -0500

    init commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <div> 
+                <form>
+                    <input>
+
+                    </input>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchBar
