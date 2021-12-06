ebdeb14ff023f54a15da5c0430c5704ded3f255a
justicemubayiwa <j.mubayiwa@gmail.com>
2021-12-06 00:11:11 -0500

    render images in grid

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import axios from 'axios'
+import unsplash from '../api/unsplash'
 import SearchBar from './SearchBar'
+import ImageList from './ImageList'
 
 
 class App extends React.Component{
@@ -8,13 +9,10 @@ class App extends React.Component{
     state = { images: []}
 
     onSearchSubmit = async term => {
-        const response = await axios
-            .get('https://api.unsplash.com/search/photos',{
+        const response = await unsplash.get('/search/photos',{
                 params: {query: term},  
-                headers: {
-                    Authorization: 'Client-ID OkKpSYwzSXjBWtIW6e6JaK82q4rsQWYdbG5-jXlNCPA'
-                }  
             }) 
+
                 this.setState({images: response.data.results})
     }
     
@@ -22,7 +20,7 @@ class App extends React.Component{
         return (
             <div className='ui container' style={{marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
-                Found: {this.state.images.length} images 
+                <ImageList images={this.state.images}  />
             </div>
         )
     }
